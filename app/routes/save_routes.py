828435bb28baeb544bf403937cb7038715413a08
Hannah <hannahrwatkins@gmail.com>
2023-08-18 16:18:21 -0400

    removed flask login, updated routes

--- app/routes/save_routes.py
+++ app/routes/save_routes.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, request, jsonify, current_app
-from flask_login import login_required, current_user
 from app.models.save import SavedVerse
 from app import db
+from app.models.user import User
+
 
 save_bp = Blueprint('save', __name__)
 
 @save_bp.route('/saved_verses', methods=['GET'])
-@login_required
 def get_saved_verses():
-    saved_verses = SavedVerse.query.filter_by(user_id=current_user.id).all()
+    saved_verses = SavedVerse.query.filter_by(user_id=User.id).all()
 
     saved_verses_data = [
         {
@@ -22,14 +22,13 @@ def get_saved_verses():
 
 
 @save_bp.route('/save', methods=['POST'])
-@login_required
 def save_verse():
     verse_text = request.json.get('verse_text')
     
     if not verse_text:
         return jsonify({'message': 'Verse text is required'}), 400
 
-    saved_verse = SavedVerse(user_id=current_user.id, verse_text=verse_text)
+    saved_verse = SavedVerse(user_id=User.id, verse_text=verse_text)
     db.session.add(saved_verse)
     db.session.commit()
     
@@ -37,11 +36,10 @@ def save_verse():
 
 
 @save_bp.route('/saved_verses/<int:verse_id>', methods=['DELETE'])
-@login_required
 def delete_saved_verse(verse_id):
     saved_verse = SavedVerse.query.get_or_404(verse_id)
 
-    if saved_verse.user_id != current_user.id:
+    if saved_verse.user_id != User.id:
         return jsonify({'message': 'Unauthorized'}), 403
 
     db.session.delete(saved_verse)
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,15 +1,54 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required, current_user
+from flask import Blueprint, request, jsonify, make_response
 from app.models.user import User
+from app import db
 
-user_bp = Blueprint('user', __name__)
 
-@user_bp.route('/profile', methods=['GET'])
-@login_required
-def get_user_profile():
-    user_data = {
-        'id': current_user.id,
-        'username': current_user.username,
-        'saved_verses': current_user.saved_verses,
-    }
-    return jsonify(user_data)
+user_bp = Blueprint("user", __name__, url_prefix="/user")
+
+@user_bp.route("/login", methods=["POST"])
+def login_user():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    
+    if not username or not password:
+        return make_response(jsonify({"message": "Username and password are required."}), 400)
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user or not user.check_password(password):
+        return make_response(jsonify({"message": "Invalid username or password."}), 401)
+
+    return jsonify({"message": "Login successful.", "user": user.to_dict()}), 200
+
+
+@user_bp.route("/register", methods=["POST"])
+def register_user():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    first_name = data.get("firstName")
+    last_name = data.get("lastName")
+
+    if not username or not password or not first_name or not last_name:
+        return make_response(jsonify({"message": "Username, password, first name, and last name are required."}), 400)
+
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return make_response(jsonify({"message": "Username already taken."}), 409)
+
+    new_user = User(username=username, first_name=first_name, last_name=last_name)
+    new_user.set_password(password)  
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(new_user.to_dict()), 201
+
+# @user_bp.route("/profile", methods=["GET"])
+# def get_user_profile():
+#     user_data = {
+#         "id": 1,
+#         "username": "Goldfish",
+#         "saved_verses": "Luke",
+#     }
+#     return jsonify(user_data)
