ac8da7d6a2e7b7a014939c754009289d0fd54db6
Hannah1Watkins <111942870+Hannah1Watkins@users.noreply.github.com>
2023-08-18 03:48:59 -0400

    Update main_routes.py

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, jsonify
 
-# Create a Blueprint for your routes
 main_bp = Blueprint("main", __name__)
 
-# Define a route for the root URL ("/")
 @main_bp.route("/")
 def index():
     return jsonify(message="Welcome to the Bible Verse App!")
